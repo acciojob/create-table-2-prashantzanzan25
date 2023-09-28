d4b5bee5a441af12c3c8d4e4aa5512012b3eeffe
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-28 12:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
-    //Write your code here
-  
+    const numRows = parseInt(prompt("Input number of rows"));
+	const numCols = parseInt(prompt("Input number of columns"));
+	const table = document.getElementById("myTable");
+
+    table.innerHTML = '';
+	for (let i = 0; i < numRows; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < numCols; j++) {
+            const cell = document.createElement("td");
+			
+			cell.textContent = `Row-${i} Column-${j}`;
+			
+			row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
 }
+
